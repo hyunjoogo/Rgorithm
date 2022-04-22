7eb7be75e3e7a9ce9c415015fe9a913bbb9ab3a3
HyunJoo Kim <hyunjoogo@gmail.com>
2022-04-23 01:41:21 +0900

    Update 9012-괄호.py

--- WEEK2/Joo/알고리즘_기초_1/200_자료구조1/9012-괄호.py
+++ WEEK2/Joo/알고리즘_기초_1/200_자료구조1/9012-괄호.py
@@ -1,1 +1,14 @@
 # https://www.acmicpc.net/problem/9012
+
+for _ in range(int(input())):
+    PS = input()
+    stack =[]
+    for j in PS:
+        if j == '(':
+            stack.append(j)
+        elif j == ')':
+            if not stack or stack[-1] != "(":
+                stack.append(")")
+                break
+            stack.pop()
+    print("YES" if not stack else "NO")
