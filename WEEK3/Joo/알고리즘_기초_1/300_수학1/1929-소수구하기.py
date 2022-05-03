a4d63ba4403ea448d58929d25f9adc60b8318b9f
HyunJoo Kim <hyunjoogo@gmail.com>
2022-05-04 02:06:18 +0900

    Update 1929-소수구하기.py

--- WEEK3/Joo/알고리즘_기초_1/300_수학1/1929-소수구하기.py
+++ WEEK3/Joo/알고리즘_기초_1/300_수학1/1929-소수구하기.py
@@ -1,5 +1,26 @@
 # https://www.acmicpc.net/problem/1929
 
+## Good Coder
+
+def primeList(n):
+    s = [True] * (n+1)
+
+    for i in range(2, int((n) ** 0.5) + 1):
+        if s[i]:
+            for j in range(i+i, n+1, i):
+                s[j] = False
+
+    return [i for i in range(2,n+1) if s[i]]
+
+a, b = map(int, input().split())
+l = primeList(b)
+
+for i in l:
+    if i >= a:
+        print(i)
+
+
+# ME!
 m, n = map(int, input().split())
 
 for i in range(m, n+1):
@@ -22,7 +43,7 @@ for i in range(m, n+1):
     # 어차피 인트로 둘렀으니 (range가 정수만 받음)
     # 대충 루트2가 1.414 , 루트 4가 2, 루트9가 3이런꼴
     # 자.... 외우지말고 찾아쓰자
-    
+
     # i = 13 , j = 2 , range(2, 4) => 통과
     # i = 13 , j = 3 , range(2, 4) => 통과
     # i = 14 , j = 2 , range(2, 4) => 브레이크
