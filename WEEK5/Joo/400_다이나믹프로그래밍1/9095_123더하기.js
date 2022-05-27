7434cb21299fe6c1de10577abb4a8fdaefe9205d
HyunJoo Kim <hyunjoogo@gmail.com>
2022-05-27 14:19:48 +0900

    Update 9095_123더하기.js

--- WEEK5/Joo/400_다이나믹프로그래밍1/9095_123더하기.js
+++ WEEK5/Joo/400_다이나믹프로그래밍1/9095_123더하기.js
@@ -1,3 +1,4 @@
+// https://www.acmicpc.net/problem/9095
 const fs = require("fs");
 const input = (
   process.platform === "linux"
@@ -25,12 +26,11 @@ for (let n of input) {
 }
 console.log(result.join("\n"));
 
-// 이전 케이스에 추가적으로 무언가가 붙을 경우 
+// 이전 케이스에 추가적으로 무언가가 붙을 경우
 // dp[n] = dp[n-1] + 알파
 // 이런식으로 점화식이 만들어지는 듯하다.
 
 // 대부분 케이스 1,2,3은 거의 껌으로 나오고
-// 케이스 4~7까지가 복잡한데 
+// 케이스 4~7까지가 복잡한데
 // 연습장에 알아볼 수 있게 크게 줄을 맞춰서 하면 규칙을 찾을 수 있을 듯하다.
 // 만약 코테에서 만난다면 시간잡아먹기 딱 좋을 것 같다.
-
