bb36c5fbdeb4425d4175d897f5c9950b84f257bd
HyunJoo Kim <73145963+hyunjoogo@users.noreply.github.com>
2022-07-05 14:07:35 +0900

    Create 버블정렬.js

---
+++ WEEK11/JOO/강의/버블정렬.js
@@ -0,0 +1,40 @@
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // SWAP
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// 최적화
+// 정렬이 모두 되었을 때 (아직 남아있지만)
+// swap이 한번도 발생하지 않으면 정렬이 되었다고 판단하고
+// for문을 빠져나간다.
+function bubbleSort(arr) {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // SWAP
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+
+  return arr;
+}
+
+bubbleSort([22, 46, 24, 1, 3, 78, 4]);
+
+console.log(bubbleSort);
