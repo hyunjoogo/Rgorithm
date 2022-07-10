41140d4796e648d89cd69749711ef6b5ffed8e4c
HyunJoo Kim <73145963+hyunjoogo@users.noreply.github.com>
2022-07-10 19:47:28 +0900

    Update 합병정렬.js

--- WEEK11/JOO/강의/합병정렬.js
+++ WEEK11/JOO/강의/합병정렬.js
@@ -1,4 +1,5 @@
-/** 배열이 정렬되어 있다고 가정하고 정렬된 배열 두 개의 조합을 반환
+/** 배열 합병
+ * 배열이 정렬되어 있다고 가정하고 정렬된 배열 두 개의 조합을 반환
  * 1. 결과값 배열을 만들고 각 입력 배열에서 가장 작은 값을 확인한다.
  * 2. i, j 는 각 배열의 인덱스를 가르킨다.
  * 3. i, j가 각 배열의 끝까지 도달하기 전까지 (while문)
@@ -38,4 +39,35 @@ function merge(arr1, arr2) {
   console.log(results, i, j);
   return results;
 }
-merge([1, 10, 50], [2, 14, 99, 100]);
+
+
+
+/** 합병 정렬
+ * 1. 재귀함수로 길이가 1 또는 0이 될 때까지 합병 정렬을 호출한다.
+ * 1.1 배열의 중간점을 잡아서 slice로 나눈다.
+ * 1.2 왼쪽 배열과 오른쪽 배열을 합병 정렬 함수를 재귀시킨다.
+ * 2. 길이가 1 또는 0이 될 때까지 나누어지면 배열 합병 함수로
+ * */
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+// 중간 지점을 찾은 다음 0부터 중간 지점까지의 왼쪽을 합병 정렬을 호출
+//
+// --------- mergeSort([10, 24, 76, 73])
+// mergeSort([10, 24]) => [10, 24]
+// mergeSort([10]) / mergeSort([24])
+// [10] merge [24]
+
+// mergeSort([76, 73]) => [73, 76]
+// mergeSort([76]) / mergeSort([73])
+// [76] merge [73]
+
+// [10, 24, 73, 76]
+
+mergeSort([10, 24, 76, 73]);
