459be7d97b8405295a9e763fd0d606c272d280b8
HyunJoo Kim <73145963+hyunjoogo@users.noreply.github.com>
2022-07-13 01:20:14 +0900

    매번 조합문제만 나오면 버벅버벅

---
+++ WEEK12/Joo/프로그래머스/조합.js
@@ -0,0 +1,15 @@
+function solution(str, num) {
+  let combination = []
+  for (let i = 0; i < str.length; i++) {
+
+  }
+
+}
+
+solution('ABC', 2)
+
+// AB, AC
+// A를 고정하고 한 글자씩 넣어주기
+
+// B를 고정하고
+// BA, BC
