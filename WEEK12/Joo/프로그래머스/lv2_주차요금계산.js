83e79071a02608116f1816b00e2ec10e3bcb02e6
HyunJoo Kim <73145963+hyunjoogo@users.noreply.github.com>
2022-07-16 14:12:37 +0900

    🎉주차요금계산 풀이 완료

--- WEEK12/Joo/프로그래머스/lv2_주차요금계산.js
+++ WEEK12/Joo/프로그래머스/lv2_주차요금계산.js
@@ -16,15 +16,42 @@
 // 1 . 해당차량의 하루 누적 주차 시간을 계산한다.
 // 1회 주차 시간 : 출차시간 - 입차시간
 
-/*
-* 5961 : 
-*
-*
-* */
-
-
-
 // 2. 누적주차시간에 따른 요금을 측정한다.
 
 // 2.1 기본요금 +  (누적주차시간 - 기본시간) / 단위시간 * 단위요금
 // 2.2 단 (누적주차시간 - 기본시간) / 단위시간은 올림을 한다.
+
+
+function solution(fees, records) {
+  const [basicTime, basicFee, unitTime, unitFee] = fees;
+  const list = {}
+  for (let i of records) {
+    const record = i.split(' ');
+
+    const time = record[0].split(':').map(v=>+v)
+    const min = time[0] === 0 ? time[1] : time[0] * 60 + time[1]
+
+    if (list[record[1]]) {
+      list[record[1]].push({time : min, history : record[2]})
+    } else {
+      list[record[1]] = []
+      list[record[1]].push({time : min, history : record[2]})
+    }
+  }
+
+
+  const result = Object.keys(list).sort().map((value) => {
+    console.log(value, list[value])
+    let cumulativeTime=0
+    for (let i = 0; i < list[value].length; i = i+2) {
+      cumulativeTime += list[value][i+1] ? list[value][i+1].time - list[value][i].time : 1439 - list[value][i].time
+    }
+    console.log(cumulativeTime)
+    if (cumulativeTime < basicTime) {
+      return basicFee
+    } else {
+      return basicFee + Math.ceil((cumulativeTime - basicTime) / unitTime) * unitFee
+    }
+  })
+  return (result)
+}
