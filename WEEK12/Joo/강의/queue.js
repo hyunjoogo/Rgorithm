d9c4a6e34c8552e44de4b73912fe549421d54de0
HyunJoo Kim <73145963+hyunjoogo@users.noreply.github.com>
2022-07-18 00:34:18 +0900

    queue

---
+++ WEEK12/Joo/강의/queue.js
@@ -0,0 +1,42 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enQueue(value) {
+    const newNode = new Node(value);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+
+  }
+
+  deQueue() {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next
+    this.size--;
+
+    return temp.value;
+
+  }
+
+}
+
