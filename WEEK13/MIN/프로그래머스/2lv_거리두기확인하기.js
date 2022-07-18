f2b49a38b2d3199b23b299f6efdc07337ba6f112
Miintoo <xpeed96@gmail.com>
2022-07-18 20:12:26 +0900

    거리두기 확인문제 불완전 제출

---
+++ WEEK13/MIN/프로그래머스/2lv_거리두기확인하기.js
@@ -0,0 +1,132 @@
+function solution(places) {
+  const answer = [];
+  lab: for (const place of places) {
+    // 전체위치에서 P가 있는지 판단
+    for (let i = 0; i < place.length; i++) {
+      for (let j = 0; j < place.length; j++) {
+        if (place[i][j] === "P") {
+          // P인경우 해당 사람이 맨해튼 거리가 2이하인지 판단
+          if (!isDistance(place, i, j)) {
+            answer.push(0);
+            continue lab;
+          }
+        }
+      }
+    }
+    answer.push(1);
+  }
+  return answer;
+}
+
+function isDistance(place, i, j) {
+  // 가로줄 판단
+  for (let w = 0; w < 5; w++) {
+    if (w === j) continue;
+    if (place[i][w] === "P") {
+      let distance = Math.abs(w - j);
+      if (distance === 1) {
+        return false;
+      } else if (distance === 2) {
+        if (place[i][(w + j) / 2] === "X") {
+          continue;
+        } else {
+          return false;
+        }
+      } else {
+        continue;
+      }
+    }
+  }
+
+  // 세로줄 판단
+  for (let h = 0; h < 5; h++) {
+    if (h === i) continue;
+    if (place[h][j] === "P") {
+      let distance = Math.abs(h - i);
+      if (distance === 1) {
+        return false;
+      } else if (distance === 2) {
+        if (place[(h + i) / 2][j] === "X") {
+          continue;
+        } else {
+          return false;
+        }
+      } else {
+        continue;
+      }
+    }
+  }
+
+  // 대각선 판단
+
+  if (i >= 1 && j >= 1) {
+    if (place[i + 1][j + 1] === "P") {
+      if (place[i + 1][j] === "X" && place[i][j + 1] === "X") {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    console.log(place[i - 1][j - 1]);
+    if (place[i - 1][j - 1] === "P") {
+      if (place[i - 1][j] === "X" && place[i][j - 1] === "X") {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    if (place[i + 1][j - 1] === "P") {
+      if (place[i - 1][j] === "X" && place[i][j + 1] === "X") {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    if (place[i - 1][j + 1] === "P") {
+      if (place[i + 1][j] === "X" && place[i][j - 1] === "X") {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  } else {
+    if (i === 0 && j === 0) {
+      if (place[i + 1][j + 1] === "X") {
+        return true;
+      } else {
+        return false;
+      }
+    } else if (i === 0) {
+      if (place[i + 1][j + 1] === "P") {
+        if (place[i + 1][j] === "X" && place[i][j + 1] === "X") {
+          return true;
+        } else {
+          return false;
+        }
+      }
+      if (place[i + 1][j - 1] === "P") {
+        if (place[i - 1][j] === "X" && place[i][j + 1] === "X") {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    } else if (j === 0) {
+      if (place[i + 1][j + 1] === "P") {
+        if (place[i + 1][j] === "X" && place[i][j + 1] === "X") {
+          return true;
+        } else {
+          return false;
+        }
+      }
+      if (place[i - 1][j + 1] === "P") {
+        if (place[i + 1][j] === "X" && place[i][j - 1] === "X") {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
