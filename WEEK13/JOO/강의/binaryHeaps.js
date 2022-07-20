26e70037fb8d9188c7b577d19d21833718648059
HyunJoo Kim <73145963+hyunjoogo@users.noreply.github.com>
2022-07-20 13:49:19 +0900

    extractMax()

--- WEEK13/JOO/강의/binaryHeaps.js
+++ WEEK13/JOO/강의/binaryHeaps.js
@@ -4,7 +4,7 @@
 
 class MaxBinaryHeap {
   constructor() {
-    this.values = [];
+    this.values = [41, 39, 33, 18, 27, 12];
   }
 
   insert(element) {
@@ -25,11 +25,55 @@ class MaxBinaryHeap {
       idx = parentIndex;
     }
   }
+
+  extractMax() {
+    //Swap the first value in the values property with the last one
+    // Pop from the values property, so you can return the value at the end;
+    const max = this.values[0];
+    const end = this.values.pop();
+    this.values[0] = end;
+    return max;
+
+    this.sinkDown();
+
+  }
+
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length
+    const element = this.values[idx];
+
+
+    while (true) {
+      let leftChildIndex = 2 * idx + 1;
+      let rightChildIndex = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex]
+        if (leftChild > element) {
+          swap = leftChildIndex
+        }
+      }
+      if (rightChildIndex < length ) {
+        rightChild = this.values[rightChildIndex]
+        if ((swap === null && rightChild > element) || (swap !== null && rightChild > leftChild)) {
+          swap = rightChildIndex;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+    }
+  }
 }
+
+const h = new MaxBinaryHeap();
+h.extractMax();
 
 // [41, 39, 33, 18, 27, 12, 55]
 //   0   1   2   3   4   5   6
-
+// [ 12, 39
 //            41
 //      39            33
 // 18     27       12     55
