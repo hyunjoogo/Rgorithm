f2af0fab33fbd7bb961994c6626d0533551e7246
HyunJoo Kim <73145963+hyunjoogo@users.noreply.github.com>
2022-07-23 16:46:27 +0900

    graph의 정점 삭제, 간선 삭제

--- WEEK13/JOO/강의/graph.js
+++ WEEK13/JOO/강의/graph.js
@@ -1,16 +1,43 @@
 class Graph {
   constructor() {
-    this.adjacencyList = {};
+    this.adjacencyList = {
+      "Tokyo": ["Dallas", "Hong Kong"],
+      "Dallas": ["Tokyo", "Aspen", "Hong Kong", "Los Angeles"],
+      "Aspen": ["Dallas"],
+      "Hong Kong": ["Tokyo", "Dallas", "Los Angeles"],
+      "Los Angeles": ["Hong Kong", "Dallas"]
+    };
   }
 
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
 
+  // 무방향 그래프일 경우 두 개의 정점에 대해서 추가해줘야한다.
   addEdge(vertex1, vertex2) {
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
+  }
 
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(value => value !== vertex2);
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(value => value !== vertex1);
+  }
+
+  removeVertex(vertex) {
+    // for (let i of this.adjacencyList[vertex]) {
+    //   this.removeEdge(vertex, i)
+    // }
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
   }
 
 }
+
+const g = new Graph();
+
+g.removeVertex('Tokyo');
+console.log(g);
