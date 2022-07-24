4c6761a040522cf1ac80c891625d601a5ab85cb6
HyunJoo Kim <73145963+hyunjoogo@users.noreply.github.com>
2022-07-25 00:54:24 +0900

    시간 초과

--- WEEK13/JOO/백준/10451_순열사이클.js
+++ WEEK13/JOO/백준/10451_순열사이클.js
@@ -5,8 +5,8 @@ const input = (
     : `2
 8
 3 2 7 8 1 4 5 6
-10
-2 1 3 4 5 6 7 9 10 8`
+5
+1 2 3 4 5`
 )
   .trim()
   .split("\n");
@@ -14,56 +14,34 @@ const input = (
 let testNum = +input.shift();
 
 while (testNum--) {
+  let size = +input.shift();
+  let list = input.shift().split(' ').map(v => +v);
+  // 8
+  // [ 3, 2, 7, 8, 1, 4, 5, 6]
 
-    let size = +input.shift();
-    let list = input.shift().split(' ').map(v => +v);
-    // 8
-    // [ 3, 2, 7, 8, 1, 4, 5, 6]
-
-    const graph = {};
-    for (let i = 0; i < size; i++) {
-      if (!graph[i + 1]) graph[i + 1] = [];
-      graph[i + 1] = list[i];
+  const graph = {};
+  for (let i = 0; i < size; i++) {
+    if (!graph[i + 1]) graph[i + 1] = [];
+    graph[i + 1] = list[i];
+  }
+  
+  function search(start) {
+    function cycle(value) {
+      if (graph[value] === start) return delete graph[value];
+      let temp = graph[value];
+      delete graph[value];
+      cycle(temp);
     }
 
+    cycle(start);
+  }
 
-    // graph idx
-    // 1 => 3 => 7 => 5
-    // value
-    // 3 => 7 => 5 => 1
-    // 1로 가니까 undefined
-    // 순열사이클 하나 종료
-
-    function search(start) {
-      function cycle(value) {
-        if (graph[value] === start) return delete graph[value];
-        let temp = graph[value]
-        delete graph[value];
-        cycle(temp)
-      }
-      cycle(start)
-    }
-
-    let count = 0;
-    for (let i = 1; i < size; i++) {
-      if (Object.keys(graph).length === 0) break;
-      if (!graph[i]) continue;
-      search(graph[i]);
-      count++;
-    }
-    console.log(count);
-
-    // 2로 가자
-    // idx
-    // 2
-    // value
-    // 2
-    // 2로 가니까 undefined
-    // 순열사이클 하나 종료
-
-    // 3 undefined
-    // 4
-
-
-
+  let count = 0;
+  for (let i = 1; i <= size; i++) {
+    if (Object.keys(graph).length === 0) break;
+    if (!graph[i]) continue;
+    search(graph[i]);
+    count++;
+  }
+  console.log(count);
 }
